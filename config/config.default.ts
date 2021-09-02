5660c27f0e44127fe2e2b0a5cb0d69558b133084
zhangbowy <1751170889@qq.com>
2021-09-02 20:31:48 +0800

    feat: 首次提交

---
+++ config/config.default.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+import { EggAppConfig, PowerPartial } from 'egg';
+
+export default function(appInfo: EggAppConfig) {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  config.keys = appInfo.name + '123123';
+
+  config.middleware = [ 'uuid' ];
+
+  config.security = {
+    csrf: {
+      ignore: '123',
+    },
+  };
+
+  config.customLoader = {
+    model: {
+      directory: 'app/model',
+      inject: 'app',
+      caseStyle: 'upper',
+    },
+  };
+
+  config.redis = {
+    client: {
+      port: 6379,          // Redis port
+      host: '127.0.0.1',   // Redis host
+      password: '',
+      db: 0,
+    },
+  };
+
+  const bizConfig = {
+    local: {
+      msg: 'local',
+    },
+
+    uuid: {
+      name: 'ebuuid',
+      maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+    },
+  };
+
+  return {
+    ...config as {},
+    ...bizConfig,
+  };
+}
